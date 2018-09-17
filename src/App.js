76fcd0d8dfabf8fcce6747908162f2104f786199
goksel eryigit <geryit@gmail.com>
2018-09-17 13:43:31 -0700

    deleted

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+//
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text>Opennn up App.js to start working on your app!</Text>
+//         <Text>Changes you make will automatically reload.</Text>
+//         <Text>Shake your phone to open the developer menu.</Text>
+//       </View>
+//     );
+//   }
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up src/App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { createStackNavigator } from 'react-navigation'; // Version can be specified in package.json
+
+class HomeScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Home',
+    };
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Home Screen</Text>
+                <Button
+                    title="Go to Details"
+                    onPress={() => {
+                        /* 1. Navigate to the Details route with params */
+                        this.props.navigation.navigate('Details', {
+                            itemId: 86,
+                            otherParam: 'anything you want here',
+                        });
+                    }}
+                />
+            </View>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+class DetailsScreen extends React.Component {
+    static navigationOptions = ({ navigation }) => {
+        return {
+            title: navigation.getParam('otherParam', 'A Nested Details Screen'),
+            headerStyle: {
+                backgroundColor: '#f4511e',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                fontWeight: 'bold',
+            },
+        };
+    };
+    render() {
+        const { navigation } = this.props;
+        const itemId = navigation.getParam('itemId', 'NO-ID');
+        const otherParam = navigation.getParam('otherParam', 'some default value');
+
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Button
+                    title="Update the title"
+                    onPress={() => this.props.navigation.setParams({otherParam: 'Updated!'})}
+                />
+                <Text>Details Screen</Text>
+                <Text>{JSON.stringify(this.props,undefined, 2)}</Text>
+                <Text>itemId: {itemId}</Text>
+                <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+                <Button
+                    title="Go to Details... again"
+                    onPress={() => this.props.navigation.push('Details')}
+                />
+            </View>
+        );
+    }
+}
+
+// export default createStackNavigator({
+//     Home: {
+//         screen: HomeScreen,
+//     },
+// });
+
+const RootStack = createStackNavigator(
+    {
+        Home: HomeScreen,
+        Details: DetailsScreen,
+    },
+    {
+        initialRouteName: 'Home',
+    }
+);
+
+export default class App extends React.Component {
+    render() {
+        return <RootStack />;
+    }
+}
